a3a9e0b88a9d82127ab84aa056117a9d4b4a2be0
Talha-35 <67706755+Talha-35@users.noreply.github.com>
2021-01-20 16:03:14 +0100

    created serializer classes

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog_app.apps.BlogAppConfig',
-    # 'blog_app',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
